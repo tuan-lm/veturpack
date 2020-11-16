14a2d954f94819701b5354e839189c90115ed831
tuan_lm <tuan-lm@blockhive.ee>
2020-11-16 18:04:39 +0700

    repoduce issue

--- src/OrtherFile.vue
+++ src/OrtherFile.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-    <orther-file>
+
   </div>
 </template>
 
 <script lang="ts">
 import * as _ from 'lodash'
-import OrtherFile from './OrtherFile.vue'
 
 export default {
-  components: { OrtherFile },
   props: ['foo', 'bar']
 }
 </script>
---
+++ src/OrtherFileWithClass.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+      
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  name: 'OrtherFileWithClass',
+  components: {}
+})
+export default class OrtherFileWithClass extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
